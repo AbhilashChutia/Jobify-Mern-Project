1ccf87cad3cffe6c2a6b3b625f2806e232ad2722
Abhilash <abhilashchutia1999@gmail.com>
2025-01-22 04:42:03 +0530

    Late night work

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ import mongoose from "mongoose";
 
 // Routes
 import jobRouter from "./routes/jobRouter.js";
+import authRouter from "./routes/authRouter.js";
 
 // Middleware
 import errorHandlerMiddleware from "./middleware/errorHandlerMiddleware.js";
@@ -25,6 +26,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/v1/jobs", jobRouter);
+app.use("/api/v1/auth", authRouter);
 
 app.use("*", (req, res) => {
     res.status(404).json({ msg: "route not found" });
