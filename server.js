53bc983200a28c819c00e56a9d775fd9ef4e7a95
Abhilash <abhilashchutia1999@gmail.com>
2025-01-24 03:20:15 +0530

    Late night 24th jan work

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 
 import morgan from "morgan";
 import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
 
 // Routes
 import jobRouter from "./routes/jobRouter.js";
@@ -14,18 +15,20 @@ import authRouter from "./routes/authRouter.js";
 
 // Middleware
 import errorHandlerMiddleware from "./middleware/errorHandlerMiddleware.js";
+import { authenticateUser } from "./middleware/authMiddleware.js";
 
 if (process.env.NODE_ENV === "development") {
     app.use(morgan("dev"));
 }
 
+app.use(cookieParser());
 app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("Hello World");
 });
 
-app.use("/api/v1/jobs", jobRouter);
+app.use("/api/v1/jobs", authenticateUser, jobRouter);
 app.use("/api/v1/auth", authRouter);
 
 app.use("*", (req, res) => {
