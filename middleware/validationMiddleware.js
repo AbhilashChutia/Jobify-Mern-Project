ecce877f72a6b009f448fa82553eb69635aab347
Abhilash <abhilashchutia1999@gmail.com>
2025-01-25 00:20:25 +0530

    25th Jan Early 12:20 am

--- middleware/validationMiddleware.js
+++ middleware/validationMiddleware.js
@@ -8,7 +8,7 @@ import {
 import { JOB_STATUS, JOB_TYPE } from "../utils/constants.js";
 import mongoose from "mongoose";
 import Job from "../models/JobModel.js";
-import User from "../models/userModel.js";
+import User from "../models/UserModel.js";
 
 const withValidationErrors = (validateValues) => {
     return [
@@ -91,4 +91,21 @@ export const validateLoginInput = withValidationErrors([
         .isEmail()
         .withMessage("invalid email format"),
     body("password").notEmpty().withMessage("password is required"),
+]);
+
+export const validateUpdateUserInput = withValidationErrors([
+    body("name").notEmpty().withMessage("name is required"),
+    body("lastName").notEmpty().withMessage("last name is required"),
+    body("email")
+        .notEmpty()
+        .withMessage("email is required")
+        .isEmail()
+        .withMessage("invalid email format")
+        .custom(async (email, { req }) => {
+            const user = await User.findOne({ email });
+            if (user && user._id.toString() !== req.user.userId) {
+                throw new BadRequestError("email already exists");
+            }
+        }),
+    body("location").notEmpty().withMessage("location is required"),
 ]);
--- models/userModel.js
+++ models/userModel.js
@@ -19,4 +19,10 @@ const UserSchema = new mongoose.Schema({
     },
 });
 
-export default mongoose.model("User", UserSchema);
+UserSchema.methods.toJSON = function () {
+    let obj = this.toObject();
+    delete obj.password;
+    return obj;
+};
+
+export default mongoose.models.User || mongoose.model("User", UserSchema);
